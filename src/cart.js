188885611079949f043c244db3ef3175c2d621c9
Hernan <hrwgallardo@gmail.com>
2020-09-15 22:01:25 -0300

    css shopping cart

--- src/cart.js
+++ src/cart.js
@@ -7,7 +7,9 @@ let cart = [];
 
 class Ui {
   displayProducts(cart) {
-    let result = "";
+    let result = `
+      <h2>Shopping Cart</h2>
+    `;
     let cnt = 0;
     let totalItems = cart.length;
     cart.forEach((item) => {
@@ -15,22 +17,24 @@ class Ui {
       title = title[0].toUpperCase() + title.slice(1);
       description = description[0].toUpperCase() + description.slice(1);
       result += `
-      <div id="product" class="cart_card">
-                <div class="header__cart_card">
+      <div id="product" class="item">
+                <div class="item__image">
                         <img class="image" src="${image}" />
-                    <div>
+                </div>
+                <div class="item__content">
+                  <div class="info">
                         <h4 id="title" class="title">${title}</h4>
                         <p >Cost aprox: <span class="price" id="price">${price}</span></p>
                         <p id="description" class="description">Thanks for<br /> cho-cho-choosing me!</p>
-                    </div>
-                </div>
-                <div class="actions">
+                  </div>
+                  <div class="actions">
                     <button>Delete</button>
                     <button>Save for later</button>
-                </div>
+                  </div>
+                </div>  
       </div>
       `;
-      if (++cnt < totalItems) result += `<div class="bar"></div>`;
+      // if (++cnt < totalItems) result += `<div class="bar"></div>`;
     });
     containerCart.innerHTML = result;
   }
